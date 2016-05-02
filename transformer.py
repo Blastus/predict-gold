6f541cb1244eb3485e579298de54a51e696b8f2e
Stephen "Zero" Chappell <Blastus@users.noreply.github.com>
2016-05-02 00:26:46 -0500

    Adds files left out by PyCharm from previous commit.

--- transformer.py
+++ transformer.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python3
+import collections
 import csv
 import datetime
 import pathlib
@@ -30,6 +31,21 @@ DEPENDENT_COLUMN = 'value'
 DEPENDENT_VARIABLES = 'Dependent Variables'
 INDEPENDENT_COLUMN = 'Close'
 INDEPENDENT_VARIABLES = 'Independent Variables'
+CURRENT_INDEPENDENT_VARIABLES = \
+    ('PHLX Gold-Silver Index',
+     'ETFS Metal Securities Australia Ltd. ETFS Physical Gold',
+     'Hafslund ASA Series B',
+     'Korea Cast Iron Pipe Industry Co. Ltd',
+     'Dow Jones Europe Developed Markets Select Real Estate Securities Total Return Gross Index USD',
+     'Dow Jones Brookfield Global Infrastructure Composite Yield Total Return Index AUD',
+     'Rheon Automatic Machinery Co. Ltd',
+     'Latrobe Magnesium Ltd',
+     'STOXX Europe Total Market Growth Small Net Return Index USD',
+     'Dow Jones Brookfield Europe Infrastructure Total Return Index USD',
+     'Gulf Manganese Corp. Ltd',
+     'China Molybdenum Co. Ltd. A',
+     'Iridium Communications Inc. 6.75% Perp. Pfd. Conv. Series B',
+     'Shenzhen Silver Basis Technology Co. Ltd. A')
 
 HISTORICAL_START_DATE = datetime.date(2015, 1, 1)
 HISTORICAL_STOP_DATE = datetime.date(2016, 1, 1)
@@ -38,10 +54,16 @@ FIELD_NAMES = 'field_names.txt'
 
 CURRENT_START_DATE = HISTORICAL_STOP_DATE
 CURRENT_STOP_DATE = datetime.date(2016, 4, 1)
+CURRENT_OUTPUT = 'current_values.csv'
 
 
 def main():
     valid_date = is_weekday('Tuesday', 'Thursday')
+    create_historical_data(valid_date)
+    create_current_data(valid_date)
+
+
+def create_historical_data(valid_date):
     historical_dates = tuple(date_range(HISTORICAL_START_DATE,
                                         HISTORICAL_STOP_DATE,
                                         DAY))
@@ -152,6 +174,51 @@ def pivot_from_columns_to_rows(table):
     yield field_names
     for row in zip(*columns):
         yield dict(zip(field_names, row))
+
+
+def create_current_data(valid_date):
+    current_dates = tuple(date_range(CURRENT_START_DATE,
+                                     CURRENT_STOP_DATE,
+                                     DAY))
+    current_table = collections.OrderedDict()
+    # collection all historical, independent values
+    for variable in CURRENT_INDEPENDENT_VARIABLES:
+        path = pathlib.Path(INDEPENDENT_VARIABLES, variable + CSV)
+        if not path.exists():
+            raise ValueError('{} does not exist'.format(path))
+        with path.open('rt', newline='') as file:
+            table = build_table('Date', map(
+                cast_row, map(strip_all, csv.DictReader(file))))
+        historical_data = extract_data(table, current_dates,
+                                       INDEPENDENT_COLUMN)
+        if any(value is not None for value in historical_data):
+            patch_range(historical_data)
+            final_data = [item for item, date in
+                          zip(historical_data, current_dates)
+                          if valid_date(date)]
+            current_table[path.stem] = final_data
+    # collect all historical, dependent values
+    for name, item in DEPENDENT_FILES:
+        if name == 'Gold Price 2':
+            with pathlib.Path(DEPENDENT_VARIABLES, item).open(
+                    'rt', newline='') as file:
+                table = build_table('date', filter_map(
+                    cast_row, map(lower_keys, csv.DictReader(file))))
+            historical_data = extract_data(table, current_dates,
+                                           DEPENDENT_COLUMN)
+            if any(value is not None for value in historical_data):
+                patch_range(historical_data)
+                final_data = [item for item, date in
+                              zip(historical_data, current_dates)
+                              if valid_date(date)]
+                current_table[name] = final_data
+    # write out to a file
+    round_column_values(current_table, 5)
+    transposed_table = pivot_from_columns_to_rows(current_table)
+    with pathlib.Path(CURRENT_OUTPUT).open('wt', newline='') as file:
+        writer = csv.DictWriter(file, next(transposed_table))
+        writer.writeheader()
+        writer.writerows(transposed_table)
 
 
 if __name__ == '__main__':
